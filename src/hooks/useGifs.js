afc7d4cde2ecd56a9daac86a48a4396d0d848c6f
Andres Maza <opandre123@gmail.com>
2021-06-16 15:16:30 -0400

    save changes

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,41 +1,39 @@
-import {useContext, useEffect, useState} from 'react'
+import { useContext, useEffect, useState } from 'react'
 import getGifs from '../services/getGifs'
 import GifsContext from '../context/GifsContext'
 
 const INITIAL_PAGE = 0
 
-export function useGifs ({ keyword,rating } = { keyword: null }) {
+export function useGifs({ keyword, rating } = { keyword: null }) {
   const [loading, setLoading] = useState(false)
   const [loadingNextPage, setLoadingNextPage] = useState(false)
-
   const [page, setPage] = useState(INITIAL_PAGE)
-  const {gifs, setGifs} = useContext(GifsContext)
-
+  const { gifs, setGifs } = useContext(GifsContext)
   const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
-
 
   useEffect(function () {
     setLoading(true)
 
-    getGifs({ keyword: keywordToUse,rating})
+    getGifs({ keyword: keywordToUse, rating })
       .then(gifs => {
         setGifs(gifs)
         setLoading(false)
         localStorage.setItem('lastKeyword', keyword)
       })
-  }, [keyword, keywordToUse, setGifs,rating])
+  }, [keyword, keywordToUse, setGifs, rating])
 
   useEffect(function () {
+
     if (page === INITIAL_PAGE) return
 
     setLoadingNextPage(true)
 
-    getGifs({ keyword: keywordToUse, page,rating })
+    getGifs({ keyword: keywordToUse, page, rating })
       .then(nextGifs => {
         setGifs(prevGifs => prevGifs.concat(nextGifs))
         setLoadingNextPage(false)
       })
-  }, [keywordToUse, page, setGifs,rating])
+  }, [keywordToUse, page, setGifs, rating])
 
-  return {loading, loadingNextPage, gifs, setPage}
+  return { loading, loadingNextPage, gifs, setPage }
 }
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,33 +2,32 @@ import React from 'react'
 import Gif from '../../components/Gif'
 import useSingleGif from '../../hooks/useSingleGif'
 import Spinner from '../../components/Spinner'
-import {Redirect} from 'wouter'
-import {Helmet} from 'react-helmet'
-import {Link} from 'wouter'
+import { Redirect } from 'wouter'
+import { Helmet } from 'react-helmet'
 import './index.css'
 
-export default function Detail ({ params }) {
+export default function Detail({ params }) {
 
-   const {gif,isLoading,isError}=useSingleGif({id:params.id})
+  const { gif, isLoading, isError } = useSingleGif({ id: params.id })
 
-   const title=gif?gif.title:''
+  const title = gif ? gif.title : ''
 
 
-   if(isLoading)return<Spinner/>
-     
-     
+  if (isLoading) return <Spinner />
 
-   if(isError)return <Redirect to='404'/>
-   if(!gif)return null
 
-  return(    
-      <>
+
+  if (isError) return <Redirect to='404' />
+  if (!gif) return null
+
+  return (
+    <>
       <Helmet>
         <title>{title}</title>
-        <meta name="description" content={'Detail of '+title} />
+        <meta name="description" content={'Detail of ' + title} />
       </Helmet>
       <h3 className="App-title">{gif.title}</h3>
       <Gif {...gif} />
-      <br/>
+      <br />
     </>)
 }
